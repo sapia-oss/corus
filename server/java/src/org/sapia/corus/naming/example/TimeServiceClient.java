a10452b5954f93c74f734b44989bab075a573333
Yanick Duchesne <yanickduchesne@gmail.com>
2009-10-14 17:35:45 +0000

    fixed lookup
    git-svn-id: https://sapia.googlecode.com/svn/trunk/corus@187 5eff64f3-1e4c-0410-aaf5-d36480b9dcb2

--- server/java/src/org/sapia/corus/naming/example/TimeServiceClient.java
+++ server/java/src/org/sapia/corus/naming/example/TimeServiceClient.java
@@ -1,10 +1,13 @@
 package org.sapia.corus.naming.example;
 
+import java.lang.reflect.UndeclaredThrowableException;
+import java.rmi.RemoteException;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.sapia.ubik.rmi.RemoteRuntimeException;
 import org.sapia.ubik.rmi.naming.remote.RemoteInitialContextFactory;
 import org.sapia.ubik.rmi.server.Log;
 
@@ -21,18 +24,33 @@ public class TimeServiceClient {
   public static void main(String[] args) {
     try {
       Log.setInfo();
-      Properties props = new Properties();
-      props.setProperty(Context.INITIAL_CONTEXT_FACTORY, RemoteInitialContextFactory.class.getName());
-      props.setProperty(RemoteInitialContextFactory.UBIK_DOMAIN_NAME, "default");
-      props.setProperty(Context.PROVIDER_URL, "ubik://localhost:33000");
-      InitialContext ctx = new InitialContext(props);
-      TimeService ts = (TimeService)ctx.lookup("timeService");
+      TimeService ts = lookup();
+
       while(true){
-        System.out.println("Got time: " + ts.getTime());
-        Thread.sleep(2000);
+        try{
+          System.out.println("Got time: " + ts.getTime());
+          Thread.sleep(8000);
+        }catch(RemoteRuntimeException e){
+          Thread.sleep(8000);
+          // retry
+        }catch(UndeclaredThrowableException e){
+          if(e.getUndeclaredThrowable() instanceof RemoteException){
+            Thread.sleep(8000);
+            System.out.println("Server down; will retry");
+          }
+        }
       }
     } catch (Throwable t) {
       t.printStackTrace();
     }
+  }
+  
+  private static TimeService lookup() throws Exception{
+    Properties props = new Properties();
+    props.setProperty(Context.INITIAL_CONTEXT_FACTORY, RemoteInitialContextFactory.class.getName());
+    props.setProperty(RemoteInitialContextFactory.UBIK_DOMAIN_NAME, "default");
+    props.setProperty(Context.PROVIDER_URL, "ubik://localhost:33000");
+    InitialContext ctx = new InitialContext(props);
+    return (TimeService)ctx.lookup("timeService");
   }
 }
