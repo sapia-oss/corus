57fa54a45e5c6778423c56665fde606fc3c8b375
Yanick Duchesne <yanickduchesne@gmail.com>
2010-06-30 02:18:13 +0000

    added makeIdFromDate()
    git-svn-id: https://sapia.googlecode.com/svn/trunk/corus@417 5eff64f3-1e4c-0410-aaf5-d36480b9dcb2

--- client/src/main/java/org/sapia/corus/client/common/IDGenerator.java
+++ client/src/main/java/org/sapia/corus/client/common/IDGenerator.java
@@ -1,5 +1,9 @@
 package org.sapia.corus.client.common;
 
+import java.util.Calendar;
+
+import org.apache.commons.lang.StringUtils;
+
 
 /**
  * Makes unique IDs based on time and internal counter.
@@ -9,13 +13,49 @@ package org.sapia.corus.client.common;
 public class IDGenerator {
   private static int _count = 0;
 
-  public static synchronized String makeId() {
-    String id = "" + System.currentTimeMillis() + _count++;
-
-    if (_count >= 999) {
+  private static synchronized int increment(){
+    if (_count > 99) {
       _count = 0;
     }
+    _count++;
+    return _count;
+  }
+  
+  public static synchronized String makeId() {
+    return "" + System.currentTimeMillis() + increment();
+  }
+  
+  public static synchronized String makeIdFromDate(){
+    Calendar cal = Calendar.getInstance();
+    String year    = Integer.toString(cal.get(Calendar.YEAR));
+    if(year.charAt(1) != '0'){
+      year = year.substring(year.length() - 3);
+    }
+    else{
+      year = year.substring(year.length() - 2);
+    }
+    String month   = StringUtils.leftPad(Integer.toString(cal.get(Calendar.MONTH)+1), 2, '0');
+    String day     = StringUtils.leftPad(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)), 2, '0');
+    
+    cal.set(Calendar.HOUR, 0);
+    cal.set(Calendar.MINUTE, 0);
+    cal.set(Calendar.SECOND, 0);
+    
+    long elapsedSeconds = (System.currentTimeMillis()-cal.getTimeInMillis())/1000;
+    
+    String counter = StringUtils.leftPad(Long.toString(elapsedSeconds), 5, '0');
+    StringBuilder sb =  new StringBuilder();
+    sb.append(year)
+      .append(month)
+      .append(day)
+      .append(counter)
+      .append(Integer.toString(increment()));
+    return sb.toString();
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(makeId());
 
-    return id;
+    System.out.println(makeIdFromDate());
   }
 }
