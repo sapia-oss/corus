5273cf479ef23869119bd1cdfcd78ef796901f8b
Yanick Duchesne <yanickduchesne@gmail.com>
2012-10-29 10:29:24 +0000

    Now sorting ports
    git-svn-id: https://sapia.googlecode.com/svn/trunk/corus@1787 5eff64f3-1e4c-0410-aaf5-d36480b9dcb2

--- modules/client/src/test/java/org/sapia/corus/client/services/port/PortRangeTest.java
+++ modules/client/src/test/java/org/sapia/corus/client/services/port/PortRangeTest.java
@@ -1,11 +1,23 @@
 package org.sapia.corus.client.services.port;
 
+import java.util.List;
+
 import org.junit.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.sapia.corus.client.exceptions.port.PortUnavailableException;
 import org.sapia.corus.client.services.db.persistence.ClassDescriptor;
 import org.sapia.corus.client.services.db.persistence.NoSuchFieldException;
 
 public class PortRangeTest {
+  
+  private PortRange range;
+  
+  @Before
+  public void setUp() throws Exception {
+    range = new PortRange("test", 1, 5);
+  }
 
   @Test
   public void testClassDescriptor() throws Exception {
@@ -21,6 +33,42 @@ public class PortRangeTest {
     range.getFieldForName("max");
     range.getFieldForName("available");
     range.getFieldForName("active");    
+  }
+  
+  @Test
+  public void testAcquire() throws Exception{
+    int port = range.acquire();
+    Assert.assertEquals(1, port);
+    Assert.assertEquals(4, range.getAvailable().size());    
+    Assert.assertEquals(1, range.getActive().size());
+  }
+  
+  @Test
+  public void testRelease() throws Exception{
+    int port = range.acquire();
+    range.release(port);
+    Assert.assertEquals(5, range.getAvailable().size());
+    Assert.assertEquals(0, range.getActive().size());    
+  }  
+  
+  @Test(expected=PortUnavailableException.class)
+  public void testAcquireUnavailable() throws Exception{
+    for (int i = 1; i <= 5; i++) {
+      range.acquire();
+    }
+    range.acquire();
+  }
+  
+  @Test
+  public void testPortOrdering() throws Exception {
+    int port = range.acquire();
+    range.release(port);
+    
+    List<Integer> available = range.getAvailable();
+    for (int i = 1; i <= 5; i++) {
+      port = available.get(i - 1);
+      Assert.assertEquals(i, port);
+    }
   }
 
 }
