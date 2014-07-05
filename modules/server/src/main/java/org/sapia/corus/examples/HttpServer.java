92540fd79c8cdf65decd94c8ae95d99601164739
Yanick Duchesne <yanickduchesne@gmail.com>
2014-07-05 15:45:37 +0000

    Added std output
    git-svn-id: https://sapia.googlecode.com/svn/trunk/corus@2451 5eff64f3-1e4c-0410-aaf5-d36480b9dcb2

--- modules/server/src/main/java/org/sapia/corus/examples/HttpServer.java
+++ modules/server/src/main/java/org/sapia/corus/examples/HttpServer.java
@@ -38,6 +38,7 @@ public class HttpServer implements Container {
     Server server = new ContainerServer(container);
     Connection connection = new SocketConnection(server);
     SocketAddress address = new InetSocketAddress(Conf.getSystemProperties().getIntProperty(HTTP_SERVER_PORT_KEY));
+    System.out.println("Starting server on port: " + address);
     connection.connect(address);
   }
 
