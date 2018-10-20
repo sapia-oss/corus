c5a73914ce3dbc89603e64dc02a282d816f5cd81
Jean-Cedric Desrochers <jcdesrochers@gmail.com>
2018-10-20 01:02:44 -0400

    lenient on socket exception as well

--- modules/client/src/main/java/org/sapia/corus/client/services/cluster/ClusteredCommand.java
+++ modules/client/src/main/java/org/sapia/corus/client/services/cluster/ClusteredCommand.java
@@ -3,6 +3,7 @@ package org.sapia.corus.client.services.cluster;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.net.SocketException;
 import java.rmi.RemoteException;
 import java.util.Collections;
 import java.util.HashSet;
@@ -155,7 +156,7 @@ public class ClusteredCommand extends InvokeCommand implements CorusCallbackCapa
       if (nextAddress != null) {
         try {
           return doCascade(nextAddress);
-        } catch (RemoteException e) {
+        } catch (RemoteException | SocketException e) {
           if (callback.isLenient()) {
             callback.debug(
                 String.format(
